e6ce24c00776e7a996aa28f1964585d1526ca8e8
Sacha Vandermoeten <sachadu835@gmail.com>
2025-07-26 05:25:23 +0200

    chore(content): optimisation contenu + link

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,9 +3,9 @@ import clsx from 'clsx'
 
 const baseStyles = {
   solid:
-    'inline-flex justify-center rounded-lg py-2 px-3 text-sm font-semibold transition-colors',
+    'inline-flex justify-center rounded-lg py-2 px-3 text-sm font-semibold transition-colors cursor-pointer',
   outline:
-    'inline-flex justify-center rounded-lg border py-[calc(--spacing(2)-1px)] px-[calc(--spacing(3)-1px)] text-sm transition-colors',
+    'inline-flex justify-center rounded-lg border py-[calc(--spacing(2)-1px)] px-[calc(--spacing(3)-1px)] text-sm transition-colors cursor-pointer',
 }
 
 const variantStyles = {
--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -1,6 +1,6 @@
-import { AppStoreLink } from '@/components/AppStoreLink'
 import { CircleBackground } from '@/components/CircleBackground'
 import { Container } from '@/components/Container'
+import { Button } from './Button'
 
 export function CallToAction() {
   return (
@@ -14,15 +14,20 @@ export function CallToAction() {
       <Container className="relative">
         <div className="mx-auto max-w-md sm:text-center">
           <h2 className="text-3xl font-medium tracking-tight text-white sm:text-4xl">
-            Get your first tips today
+            Votre application n’attend plus
           </h2>
           <p className="mt-4 text-lg text-gray-300">
-            It takes 30 seconds to sign up. Download the app and create an
-            account today and we’ll send you a tip guaranteed to double your
-            first investment.
+            Prenez 30 secondes pour réserver un créneau gratuit. Parlons de
+            votre projet et voyons comment le concrétiser, dès aujourd’hui.
           </p>
           <div className="mt-8 flex justify-center">
-            <AppStoreLink color="white" />
+            <Button
+              variant="solid"
+              href={'https://cal.com/sacha-vandermoeten/merci-de-votre-interet'}
+              target="_blank"
+            >
+              Prendre un rendez-vous
+            </Button>
           </div>
         </div>
       </Container>
